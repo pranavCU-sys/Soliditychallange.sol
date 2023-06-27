6064f63a573ea5556c88ac232a52f334db139737
pranavCU-sys <120193688+pranavCU-sys@users.noreply.github.com>
2023-06-27 22:24:44 +0530

    Create Soliditychallange2.sol

---
+++ Soliditychallange2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract SolidityC_2 {
+    
+    function Conv()public payable returns (uint256 weival, uint256 etherval, uint256 gweival) { 
+        //41293
+    
+
+        etherval = msg.value;
+        weival = msg.value;
+        gweival = msg.value;
+
+    }
+
+}
